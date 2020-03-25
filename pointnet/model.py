0dcf8c145114b5d5b7d08ddab550bf298bbb59bb
Tim Patten <timpatten.syd@gmail.com>
2020-03-25 17:59:25 +0100

    Small change to run the classifier again

--- pointnet/model.py
+++ pointnet/model.py
@@ -87,7 +87,7 @@ class STNkd(nn.Module):
 class PointNetfeat(nn.Module):
     def __init__(self, global_feat=True, feature_transform=False):
         super(PointNetfeat, self).__init__()
-        # self.stn = STN3d()
+        self.stn = STN3d()
         self.conv1 = torch.nn.Conv1d(3, 64, 1)
         self.conv2 = torch.nn.Conv1d(64, 128, 1)
         self.conv3 = torch.nn.Conv1d(128, 1024, 1)
@@ -96,26 +96,52 @@ class PointNetfeat(nn.Module):
         self.bn3 = nn.BatchNorm1d(1024)
         self.global_feat = global_feat
         self.feature_transform = feature_transform
-        # if self.feature_transform:
-        #     self.fstn = STNkd(k=64)
+        if self.feature_transform:
+            self.fstn = STNkd(k=64)
 
     def forward(self, x):
         n_pts = x.size()[2]
-        # trans = self.stn(x)
-        trans = None
-        # x = x.transpose(2, 1)
-        # x = torch.bmm(x, trans)
-        # x = x.transpose(2, 1)
-        # print("Shape 4 {}".format(x.shape))  # torch.Size([32, 3, 2500])
+        trans = self.stn(x)
+        x = x.transpose(2, 1)
+        x = torch.bmm(x, trans)
+        x = x.transpose(2, 1)
         x = F.relu(self.bn1(self.conv1(x)))
 
-        # if self.feature_transform:
-        #     trans_feat = self.fstn(x)
-        #     x = x.transpose(2, 1)
-        #     x = torch.bmm(x, trans_feat)
-        #     x = x.transpose(2, 1)
-        # else:
-        #     trans_feat = None
+        if self.feature_transform:
+            trans_feat = self.fstn(x)
+            x = x.transpose(2, 1)
+            x = torch.bmm(x, trans_feat)
+            x = x.transpose(2, 1)
+        else:
+            trans_feat = None
+
+        pointfeat = x
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.bn3(self.conv3(x))
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
+        if self.global_feat:
+            return x, trans, trans_feat
+        else:
+            x = x.view(-1, 1024, 1).repeat(1, 1, n_pts)
+            return torch.cat([x, pointfeat], 1), trans, trans_feat
+
+
+class PointNetfeat_custom(nn.Module):
+    def __init__(self, global_feat=True):
+        super(PointNetfeat_custom, self).__init__()
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.global_feat = global_feat
+
+    def forward(self, x):
+        n_pts = x.size()[2]
+        trans = None
+        x = F.relu(self.bn1(self.conv1(x)))
         trans_feat = None
 
         pointfeat = x
@@ -133,9 +159,8 @@ class PointNetfeat(nn.Module):
 class PointNetCls(nn.Module):
     def __init__(self, k=2, feature_transform=False):
         super(PointNetCls, self).__init__()
-        # self.feature_transform = feature_transform
-        # self.feat = PointNetfeat(global_feat=True, feature_transform=feature_transform)
-        self.feat = PointNetfeat(global_feat=True, feature_transform=False)
+        self.feature_transform = feature_transform
+        self.feat = PointNetfeat(global_feat=True, feature_transform=feature_transform)
         self.fc1 = nn.Linear(1024, 512)
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, k)
@@ -193,7 +218,7 @@ def feature_transform_regularizer(trans):
 class PointNetRegression(nn.Module):
     def __init__(self, k_out=3):
         super(PointNetRegression, self).__init__()
-        self.feat = PointNetfeat(global_feat=True, feature_transform=False)
+        self.feat = PointNetfeat_custom(global_feat=True, feature_transform=False)
         self.fc1 = nn.Linear(1024, 512)
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, k_out)
@@ -207,7 +232,6 @@ class PointNetRegression(nn.Module):
         x = F.relu(self.bn1(self.fc1(x)))
         x = F.relu(self.bn2(self.dropout(self.fc2(x))))
         x = self.fc3(x)
-        # return F.log_softmax(x, dim=1)
         return x
 
 
